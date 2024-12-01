db9f1343a072ae67c5378190abde63a2f7ab0e28
pduong96 <peter.duong@clio.com>
2024-11-30 22:53:01 -0700

    added chomp option to eliminate the new line character

--- day-0/day_0.rb
+++ day-0/day_0.rb
@@ -8,7 +8,7 @@ end
 
 # Test if implementation meets criteria from the description, swap out the expected value
 test_file_path = File.expand_path('test.txt', File.dirname(__FILE__))
-test_input = File.read(test_file_path).strip.lines
+test_input = File.read(test_file_path).strip.lines(chomp: true)
 
 raise 'Test failed' unless part_1(test_input) == 0
 
@@ -16,6 +16,6 @@ raise 'Test failed' unless part_1(test_input) == 0
 
 # if solution works with the test input, run with the puzzle input
 file_path = File.expand_path('input.txt', File.dirname(__FILE__))
-input = File.read(file_path).strip.lines
+input = File.read(file_path).strip.lines(chomp: true)
 puts "Result for part 1: #{part_1(input)}"
 # puts "Result for part 2: #{part_2(input)}"
